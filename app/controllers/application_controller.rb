80296c23f2d09bcde2fed19c54bc6e6b466c4995
marshall nyasha chikari <marshall.chikari@gmail.com>
2020-08-15 20:18:00 +0200

    authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def authenticate_user
+    if !user_signed_in?
+      redirect_to login_path
+    end
+  end
+
   # Confirms a logged-in user.
   def logged_in_user
     unless user_signed_in?
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user
+
   def index
     @image_items = current_user.image_feed.paginate(page: params[:page])
     @video_items = current_user.video_feed.paginate(page: params[:page])
